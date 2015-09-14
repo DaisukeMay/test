d5959ed678d545e64ba3b26828f47d9f80ec041c
Daisuke Lee <ljldaisuke@hotmail.com>
2015-09-14 11:18:15 +0800

    测试git

---
+++ test/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace test
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetWeb();
+            //测试一下Git是否正常工作
+        }
+
+        public void JustATest()
+        {
+            string a = "测试Git的功能";
+        }
+
+        public void GetWeb()
+        {
+            HttpWebRequest req = null;
+            HttpWebResponse res = null;
+
+            string strBuff = "";
+            char[] cbuffer = new char[256];
+            int byteRead = 0;
+
+            Uri URL = new Uri("http://dotamax.com/player/detail/203592444/");
+
+            try
+            {
+                req = (HttpWebRequest)WebRequest.Create(URL);
+                res = (HttpWebResponse)req.GetResponse();
+                Stream respStream = res.GetResponseStream();
+                StreamReader sr = new StreamReader(respStream, Encoding.UTF8);
+                byteRead = sr.Read(cbuffer, 0, 256);
+                while (byteRead != 0)
+                {
+                    string strResp = new string(cbuffer, 0, byteRead);
+                    strBuff = strBuff + strResp;
+                    byteRead = sr.Read(cbuffer, 0, 256);
+                }
+                StreamWriter sw = new StreamWriter(@"d:1.txt");
+                sw.Write(strBuff);
+                respStream.Close();
+                sw.Close();
+                Close();
+            }
+            catch (ProtocolViolationException e)
+            {
+                Console.WriteLine("获取网页内容失败", e);
+            }
+            catch
+            {
+                Console.WriteLine("发生错误");
+            }
+        }
+
+
+    }
+}
